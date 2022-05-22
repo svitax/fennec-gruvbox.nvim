923f2da262dcad8b38b16ad94541c7fb994efec4
Igor Barale <svitaxiom@gmail.com>
2022-05-22 11:29:49 -0700

    add line_fg and line_active_fg

--- lua/lualine/themes/fennec-gruvbox.lua
+++ lua/lualine/themes/fennec-gruvbox.lua
@@ -8,27 +8,27 @@ return {
     -- a = { bg = c.light_blue, fg = c.dark, gui = "bold" },
     a = { bg = c.line_bg, fg = c.light_blue, gui = "bold" },
     b = { bg = c.line_bg, fg = c.light_blue },
-    c = { bg = c.line_bg, fg = c.comment },
+    c = { bg = c.line_bg, fg = c.line_fg },
   },
   insert = {
     a = { bg = c.bright_yellow, fg = c.dark, gui = "bold" },
     b = { bg = c.line_bg, fg = c.bright_yellow },
-    c = { bg = c.line_bg, fg = c.comment },
+    c = { bg = c.line_bg, fg = c.line_fg },
   },
   visual = {
     a = { bg = c.milk, fg = c.dark, gui = "bold" },
     b = { bg = c.line_bg, fg = c.milk },
-    c = { bg = c.line_bg, fg = c.comment },
+    c = { bg = c.line_bg, fg = c.line_fg },
   },
   replace = {
     a = { bg = c.error_red, fg = c.dark, gui = "bold" },
     b = { bg = c.line_bg, fg = c.error_red },
-    c = { bg = c.line_bg, fg = c.comment },
+    c = { bg = c.line_bg, fg = c.line_fg },
   },
   command = {
     a = { bg = c.soft_green, fg = c.dark, gui = "bold" },
     b = { bg = c.line_bg, fg = c.soft_green },
-    c = { bg = c.line_bg, fg = c.comment },
+    c = { bg = c.line_bg, fg = c.line_fg },
   },
   inactive = {
     a = { bg = c.background, fg = c.gray, gui = "bold" },
