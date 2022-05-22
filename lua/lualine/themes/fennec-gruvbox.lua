ce83fffcdfebb4eb084d4462ada51461c00b5232
Igor Barale <svitaxiom@gmail.com>
2022-05-22 11:49:00 -0700

    revert lualine theme

--- lua/lualine/themes/fennec-gruvbox.lua
+++ lua/lualine/themes/fennec-gruvbox.lua
@@ -5,34 +5,33 @@ local c = require("fennec-gruvbox.colors").config()
 
 return {
   normal = {
-    -- a = { bg = c.light_blue, fg = c.dark, gui = "bold" },
-    a = { bg = c.line_bg, fg = c.line_active_fg, gui = "bold" },
-    b = { bg = c.line_bg, fg = c.line_fg },
-    c = { bg = c.line_bg, fg = c.line_fg },
+    a = { bg = c.light_blue, fg = c.dark, gui = "bold" },
+    b = { bg = c.background, fg = c.light_blue },
+    c = { bg = c.background, fg = c.milk },
   },
   insert = {
     a = { bg = c.bright_yellow, fg = c.dark, gui = "bold" },
-    b = { bg = c.line_bg, fg = c.bright_yellow },
-    c = { bg = c.line_bg, fg = c.line_fg },
+    b = { bg = c.background, fg = c.bright_yellow },
+    c = { bg = c.background, fg = c.milk },
   },
   visual = {
     a = { bg = c.milk, fg = c.dark, gui = "bold" },
-    b = { bg = c.line_bg, fg = c.milk },
-    c = { bg = c.line_bg, fg = c.line_fg },
+    b = { bg = c.background, fg = c.milk },
+    c = { bg = c.background, fg = c.milk },
   },
   replace = {
     a = { bg = c.error_red, fg = c.dark, gui = "bold" },
-    b = { bg = c.line_bg, fg = c.error_red },
-    c = { bg = c.line_bg, fg = c.line_fg },
+    b = { bg = c.background, fg = c.error_red },
+    c = { bg = c.background, fg = c.milk },
   },
   command = {
     a = { bg = c.soft_green, fg = c.dark, gui = "bold" },
-    b = { bg = c.line_bg, fg = c.soft_green },
-    c = { bg = c.line_bg, fg = c.line_fg },
+    b = { bg = c.background, fg = c.soft_green },
+    c = { bg = c.background, fg = c.milk },
   },
   inactive = {
-    a = { bg = c.background, fg = c.gray, gui = "bold" },
-    b = { bg = c.background, fg = c.gray },
-    c = { bg = c.background, fg = c.gray },
+    a = { bg = c.dark_gray, fg = c.gray, gui = "bold" },
+    b = { bg = c.dark_gray, fg = c.gray },
+    c = { bg = c.dark_gray, fg = c.gray },
   },
 }
