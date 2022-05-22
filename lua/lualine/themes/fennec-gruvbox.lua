6738d3063d2e59db4151f99a0452e823284598da
Igor Barale <svitaxiom@gmail.com>
2022-05-21 23:48:37 -0700

    rename lualine theme and change bg color

--- lua/lualine/themes/fennec-gruvbox.lua
+++ lua/lualine/themes/fennec-gruvbox.lua
@@ -1,33 +1,33 @@
 -- Copyright (c) 2020-2021 hoob3rt
 -- MIT license, see LICENSE for more details.
 -- stylua: ignore
-local c = require("gruvbox-baby.colors").config()
+local c = require("fennec-gruvbox.colors").config()
 
 return {
   normal = {
     a = { bg = c.light_blue, fg = c.dark, gui = "bold" },
-    b = { bg = c.background, fg = c.light_blue },
-    c = { bg = c.background, fg = c.milk },
+    b = { bg = c.line_bg, fg = c.light_blue },
+    c = { bg = c.line_bg, fg = c.milk },
   },
   insert = {
     a = { bg = c.bright_yellow, fg = c.dark, gui = "bold" },
-    b = { bg = c.background, fg = c.bright_yellow },
-    c = { bg = c.background, fg = c.milk },
+    b = { bg = c.line_bg, fg = c.bright_yellow },
+    c = { bg = c.line_bg, fg = c.milk },
   },
   visual = {
     a = { bg = c.milk, fg = c.dark, gui = "bold" },
-    b = { bg = c.background, fg = c.milk },
-    c = { bg = c.background, fg = c.milk },
+    b = { bg = c.line_bg, fg = c.milk },
+    c = { bg = c.line_bg, fg = c.milk },
   },
   replace = {
     a = { bg = c.error_red, fg = c.dark, gui = "bold" },
-    b = { bg = c.background, fg = c.error_red },
-    c = { bg = c.background, fg = c.milk },
+    b = { bg = c.line_bg, fg = c.error_red },
+    c = { bg = c.line_bg, fg = c.milk },
   },
   command = {
     a = { bg = c.soft_green, fg = c.dark, gui = "bold" },
-    b = { bg = c.background, fg = c.soft_green },
-    c = { bg = c.background, fg = c.milk },
+    b = { bg = c.line_bg, fg = c.soft_green },
+    c = { bg = c.line_bg, fg = c.milk },
   },
   inactive = {
     a = { bg = c.dark_gray, fg = c.gray, gui = "bold" },
